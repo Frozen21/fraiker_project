11d719af814a4a9d8d31cbbbfd7cb69159f1cfc0
Alexey Tarasov <alexey.tarasov@chejam.com>
2023-04-20 23:59:10 +0300

    Footer end

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -78,130 +78,83 @@ AppAsset::register($this);
         <?= $content ?>
     </div>
 </main>
-
-<!-- Footer -->
-<footer class="text-center text-lg-start bg-light text-muted">
-    <!-- Section: Social media -->
-    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-        <!-- Left -->
-        <div class="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-        </div>
-        <!-- Left -->
-
-        <!-- Right -->
-        <div>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-twitter"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-google"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-instagram"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-linkedin"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-github"></i>
-            </a>
-        </div>
-        <!-- Right -->
-    </section>
-    <!-- Section: Social media -->
-
-    <!-- Section: Links  -->
-    <section class="">
-        <div class="container text-center text-md-start mt-5">
-            <!-- Grid row -->
-            <div class="row mt-3">
-                <!-- Grid column -->
-                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                    <!-- Content -->
-                    <h6 class="text-uppercase fw-bold mb-4">
-                        <i class="fas fa-gem me-3"></i>Company name
-                    </h6>
-                    <p>
-                        Here you can use rows and columns to organize your footer content. Lorem ipsum
-                        dolor sit amet, consectetur adipisicing elit.
-                    </p>
-                </div>
-                <!-- Grid column -->
-
-                <!-- Grid column -->
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <!-- Links -->
-                    <h6 class="text-uppercase fw-bold mb-4">
-                        Products
-                    </h6>
-                    <p>
-                        <a href="#!" class="text-reset">Angular</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">React</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Vue</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Laravel</a>
-                    </p>
-                </div>
-                <!-- Grid column -->
-
-                <!-- Grid column -->
-                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                    <!-- Links -->
-                    <h6 class="text-uppercase fw-bold mb-4">
-                        Useful links
-                    </h6>
-                    <p>
-                        <a href="#!" class="text-reset">Pricing</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Settings</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Orders</a>
-                    </p>
-                    <p>
-                        <a href="#!" class="text-reset">Help</a>
-                    </p>
-                </div>
-                <!-- Grid column -->
-
-                <!-- Grid column -->
-                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                    <!-- Links -->
-                    <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
-                    <p>
-                        <i class="fas fa-envelope me-3"></i>
-                        info@example.com
-                    </p>
-                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
-                </div>
-                <!-- Grid column -->
+<footer class="my-footer mt-auto py-3 text-muted">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-1">
+                <img class="float-right footer-image" src="/images/bee_logo.png">
             </div>
-            <!-- Grid row -->
+            <div class="col-md-2">
+                <h4 class="my-navbarlabel">OOO "МЁД"</h4>
+                <p class="footer-text"> Натуральный мёд из Чувашии. Сбор только в экологически чистых местах.</p>
+            </div>
+            <div class="col-md-1">
+            </div>
+            <div class="col-md-2">
+                <h5 class="footer-text">Oсновные ссылки</h5>
+                <ul class="list-unstyled float-left">
+                    <li>
+                        <a href="/site/about" class="ul-footer-text float-left">О магазине</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left">Каталог товаров</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left">Доставка и оплата</a>
+                    </li>
+                    <li>
+                        <a href="/site/contact" class="ul-footer-text float-left">Контакты</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h5 class="footer-text">Категории</h5>
+                <ul class="list-unstyled float-left">
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left">Мёд</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left">Семена медоносных культур</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left">Прополис</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left">Побочные продукты</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h5 class="footer-text">Полезные ссылки</h5>
+                <ul class="list-unstyled float-left">
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left">О пользе мёда</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left">Как собирают мёд</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left">Наша миссия</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h5 class="footer-text">Соц. сети</h5>
+                <ul class="list-unstyled float-left">
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left"><i class="fa fa-vk"></i>VK.com</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left"><i class="fab fa-facebook-f"></i>Facebook</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="ul-footer-text float-left"><i class="fa fa-instagram"></i>Instagram</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </section>
-    <!-- Section: Links  -->
-
-    <!-- Copyright -->
-    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-        © 2021 Copyright:
-        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
     </div>
-    <!-- Copyright -->
 </footer>
-<!-- Footer -->
-
 <?php $this->endBody() ?>
 </body>
 </html>
